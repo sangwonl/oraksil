9cf32546e521dc7b462914e6a1965aa6a2d508a0
gamz <gamzabaw@gmail.com>
2023-11-02 07:40:39 +0900

    Fix broken pack image urls

--- migrations/up.sql
+++ migrations/up.sql
@@ -64,10 +64,10 @@ CREATE TABLE feedback (
 
 INSERT INTO pack (status, title, maker, description, max_players, poster_url, rom_name)
 VALUES
-    (0, 'Tekken 3', 'NAMCO', 'Tekken 3 (鉄拳3) is a fighting game, the third installment in the Tekken series.', 2, '/public/images/tekken3.png', 'tekken3'),
-    (1, 'Bobl Bubl', 'TAITO', '', 2, '/public/images/bublbobl.png', 'bublbobl'),
-    (1, 'Cadilacs Dinosours', 'Capcom', "It is a side-scrolling beat 'em up based on the comic book series Xenozoic Tales. The game was produced as a tie-in to the short-lived Cadillacs and Dinosaurs animated series which was aired during the same year the game was released.", 3, '/public/images/dino.png', 'dino'),
-    (0, 'Final Fight II', 'Capcom', '', 3, '/public/images/ffight2b.png', 'ffight2b'),
-    (1, 'Metal Slug', 'SNK', '', 2, '/public/images/mslug.png', 'mslug'),
-    (1, 'The King of Fighters 97', 'SNK', '', 2, '/public/images/kof97pls.png', 'kof97pls'),
-    (0, 'Super Tank', 'Video Games GmbH', '', 2, '/public/images/supertnk.png', 'supertnk');
+    (0, 'Tekken 3', 'NAMCO', 'Tekken 3 (鉄拳3) is a fighting game, the third installment in the Tekken series.', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/tekken3.png', 'tekken3'),
+    (1, 'Bobl Bubl', 'TAITO', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/bublbobl.png', 'bublbobl'),
+    (1, 'Cadilacs Dinosours', 'Capcom', "It is a side-scrolling beat 'em up based on the comic book series Xenozoic Tales. The game was produced as a tie-in to the short-lived Cadillacs and Dinosaurs animated series which was aired during the same year the game was released.", 3, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/dino.png', 'dino'),
+    (0, 'Final Fight II', 'Capcom', '', 3, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/ffight2b.png', 'ffight2b'),
+    (0, 'Metal Slug', 'SNK', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/mslug.png', 'mslug'),
+    (0, 'The King of Fighters 97', 'SNK', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/kof97pls.png', 'kof97pls'),
+    (1, 'Super Tank', 'Video Games GmbH', '', 2, 'https://oraksil.s3.ap-northeast-2.amazonaws.com/images/covers/supertnk.png', 'supertnk');
